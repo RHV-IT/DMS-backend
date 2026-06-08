2b6572d5ec9d5570b69ea2b869507c9b10ba8cb1
Adedara-samuel <adedrasapok@gmail.com>
2026-06-08 15:39:09 +0100

    fixes on core

--- api/src/config/cors.js
+++ api/src/config/cors.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const ALLOWED_ORIGINS = [
   "https://rhv-dms.vercel.app", // Production frontend
   "http://192.168.0.153:3000", // Local network frontend
+  "http://192.168.0.153", // Local network frontend
   "http://docmanager.rhv",      // Internal network alias
   "http://localhost:3000"       // Local development
 ];
@@ -28,14 +29,14 @@ const corsOptions = {
   credentials: true, // Enable cookies and Authorization headers in cross-origin requests
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
   allowedHeaders: [ // Allowed request headers
-    'Origin', 
-    'X-Requested-With', 
-    'Content-Type', 
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
     'Accept',
-    'Authorization', 
-    'x-browser', 
-    'x-device', 
-    'x-client-type', 
+    'Authorization',
+    'x-browser',
+    'x-device',
+    'x-client-type',
     'x-platform'
   ],
   optionsSuccessStatus: 200 // Status for successful OPTIONS requests
